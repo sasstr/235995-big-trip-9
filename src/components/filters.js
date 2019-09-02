ed268861b203d453a0aa835a232a48328deeacdb
Alexander Strakhovenko <sasstr@gmail.com>
2019-09-02 10:57:47 +0300

    Выводит данные в шаблоны 2

--- src/components/filters.js
+++ src/components/filters.js
@@ -3,7 +3,7 @@ const makeFiltersTemplate = (filtres) => `<h2 class="visually-hidden">Filter eve
     ${filtres.map((filter) => `<div class="trip-filters__filter">
       <input id="filter-${filter.id}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.id}" checked>
       <label class="trip-filters__filter-label" for="filter-${filter.id}">${filter.title}</label>
-    </div>`)}
+    </div>`).join(``)}
     <button class="visually-hidden" type="submit">Accept filter</button>
   </form>`.trim();
 
